ee1fe1de2f442588f18cd5ab85ca075f9666da8d
Jung Ho Lee <alexxleee@gmail.com>
2020-12-31 22:47:48 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,11 +3,11 @@ def using_concat(array, array2)
 end
 
 def using_insert(array, element)
-  array.insert(element,4)
+  array.insert(4, element)
 end
 
-def using_uniq
-  
+def using_uniq(array)
+  array.uniq
 end
 
 def using_flatten
