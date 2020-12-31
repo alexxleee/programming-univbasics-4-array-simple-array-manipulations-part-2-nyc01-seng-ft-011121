8360d128d1287aee4e80eb8031bdfdd7e2a24e2f
Jung Ho Lee <alexxleee@gmail.com>
2020-12-31 22:43:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,8 +1,8 @@
-def using_concat()
-  
+def using_concat(array, array2)
+  array.concat(array2)
 end
 
-def using_insert()
+def using_insert(array, )
   
 end
 
@@ -14,7 +14,7 @@ def using_flatten()
   
 end
 
-def using_delete()
+def using_delete(array, )
   
 end
 
